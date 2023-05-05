24570e4f22444654e4f9e35d9fd547d443a35252
ian-parra <86946574+ian-parra@users.noreply.github.com>
2023-05-05 18:09:20 -0300

    Add files via upload

---
+++ intro/01.intro.py
@@ -0,0 +1,10 @@
+print("Hello Word!")
+
+print("Ian" + " Ezequiel" + " Parra")
+
+input("Dime tu nombre: ")
+print("Tu Apellido es " + input("Dime tu apellido: "))
+
+print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
+
+print("El nombre de la cerveza es: \n" + input(" dame alguna palabra: ") + input("dame la segunda palabra: "))
