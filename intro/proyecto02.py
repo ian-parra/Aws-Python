2901c8d9af5a2b8e21d73e59f1d01deb4abc5711
ian-parra <24ian2002@gmail.com>
2023-05-15 23:33:34 -0300

    Delete proyecto02.py

--- intro/proyecto02.py
+++
@@ -1,7 +0,0 @@
-nombre = input("por favor diga su nombre: ")
-
-ventas = int(input("diga sus ventas totales: "))
-
-comision = round(ventas * 13 / 100, 2)
-
-print(f"Hola! {nombre} el total de comisiones de este mes es: ${comision}")
