20de3e7f25e79da62c3a7a7cec31538aef5713ae
ian-parra <24ian2002@gmail.com>
2023-05-29 17:56:04 -0300

    Update sys-admin.py

--- Introducción a la administración del sistema con Python/sys-admin.py
+++ Introducción a la administración del sistema con Python/sys-admin.py
@@ -1,10 +1,30 @@
 import os
 import subprocess
+
+# Listar archivos y directorios en el directorio actual
+os.system("dir")
+
+# Listar archivos y directorios en el directorio actual utilizando subprocess
+subprocess.run(["dir"], shell=True)
+
+# Listar archivos y directorios en formato detallado
+subprocess.run(["dir", "/w"])
+
+# Obtener información del sistema
+command = "systeminfo"
+subprocess.run([command])
+
+# Obtener información de los procesos en ejecución
+command = "tasklist"
+subprocess.run([command])
+
+"""import os
+import subprocess
 os.system("ls")
 
-#subprocess.run(["ls"])
+subprocess.run(["ls"])
 
-##subprocess.run(["ls","-l"])
+subprocess.run(["ls","-l"])
 subprocess.run(["ls","-l","README.md"])
 command="uname"
 commandArgument="-a"
@@ -13,4 +33,4 @@ subprocess.run([command,commandArgument])
 command="ps"
 commandArgument="-x"
 print(f'Gathering active process information with command: {command} {commandArgument}')
-subprocess.run([command,commandArgument])
+subprocess.run([command,commandArgument])"""
